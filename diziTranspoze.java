aab7d979f72d9c6dca2b3fe86d7dd84b0f7ec68f
Yusuf YAPAR <ysueph@gmail.com>
2023-02-09 23:50:08 +0300

    diziTranspoze

---
+++ diziTranspoze.java
@@ -0,0 +1,34 @@
+package projeler;
+
+public class diziTranspoze {
+
+    public static void main(String[] args) {
+        
+            int[][] matrix = {
+                    {1, 4, 3},
+                    {3, 11, 4},
+          
+            };
+
+            int[][] transpose = new int[matrix[0].length][matrix.length];
+
+            System.out.println("Matris: ");
+
+            for (int i = 0; i < matrix.length; i++) {
+                for (int j = 0; j < matrix[i].length; j++) {
+                    System.out.print(matrix[i][j] + " ");
+                }
+                System.out.println();
+            }
+
+            System.out.println("Transpose: ");
+
+            for (int i = 0; i < transpose.length; i++) {
+                for (int j = 0; j < transpose[i].length; j++) {
+                    transpose[i][j] = matrix[j][i];
+                    System.out.print(transpose[i][j] + " ");
+                }
+                System.out.println();
+            }
+    }
+}
